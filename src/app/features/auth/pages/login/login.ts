6641d3a674a27e105806ab2d640275826fe772f3
Sebastienpanda <sebastienprodev@gmail.com>
2025-06-27 11:53:40 +0200

    feat: finish page login

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,10 +1,24 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {Eye, EyeOff, LucideAngularModule} from 'lucide-angular';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        RouterLink,
+        LucideAngularModule
+    ]
 })
 
 export default class Login {
+    protected readonly showPassword = signal(false);
+
+    togglePassword() {
+        this.showPassword.set(!this.showPassword());
+    }
+
+    protected readonly Eye = Eye;
+    protected readonly EyeOff = EyeOff;
 }
