282caffc25c7fff4edccfbf4c2f7ab195aa26c8d
Sebastienpanda <sebastienprodev@gmail.com>
2025-06-30 18:09:28 +0200

    feat: add environnement supabase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    supabaseUrl: "https://azzrezqejokvccshkslv.supabase.co",
+    supabaseKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6enJlenFlam9rdmNjc2hrc2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzA2NTMsImV4cCI6MjA2Njg0NjY1M30.HMOCXvxFtedadCZNlhlUkRsdSP3bm0BCK6bGxTFFFt8",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    supabaseUrl: "https://azzrezqejokvccshkslv.supabase.co",
+    supabaseKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6enJlenFlam9rdmNjc2hrc2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzA2NTMsImV4cCI6MjA2Njg0NjY1M30.HMOCXvxFtedadCZNlhlUkRsdSP3bm0BCK6bGxTFFFt8",
+};
+
